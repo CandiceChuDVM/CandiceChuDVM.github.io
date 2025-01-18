02a86975878071145a90ba173e8c28452e4a5409
CandiceChuDVM <CandiceChuDVM@users.noreply.github.com>
2025-01-18 17:56:30 +0000

    Deploying to gh-pages from @ CandiceChuDVM/candicechudvm.github.io@ac03bf985447cd3f6f4dc2ecab328860ffbe5e08 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
