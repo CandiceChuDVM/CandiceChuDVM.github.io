636a8a91ee1701d159402af9a39ccd3e34e931fa
CandiceChuDVM <CandiceChuDVM@users.noreply.github.com>
2025-01-18 18:40:03 +0000

    Deploying to gh-pages from @ CandiceChuDVM/candicechudvm.github.io@50c7b03ccbccb107532f0ea0551f3f1dec029be1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "members of Chu Lab",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
